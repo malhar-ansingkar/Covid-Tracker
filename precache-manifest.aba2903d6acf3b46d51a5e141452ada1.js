7f6e287df200c42c02a1e5243f331d75f8930e9a
malhar <malharansingkar7057@gmail.com>
2021-08-13 02:11:04 +0530

    Updates

---
+++ precache-manifest.aba2903d6acf3b46d51a5e141452ada1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504826754dcca79be447b44b2d065762",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "655e46a188d1cf994c9a",
+    "url": "/Covid-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "d9c4bba6b69942e7fb18",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-Tracker/static/js/2.c3990831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655e46a188d1cf994c9a",
+    "url": "/Covid-Tracker/static/js/main.ded81401.chunk.js"
+  },
+  {
+    "revision": "ae45445b70030a69d951",
+    "url": "/Covid-Tracker/static/js/runtime-main.d4cdb983.js"
+  }
+]);
